a19b125697e7bfa392641583f28361dadd8103e8
MSGroupFM <admin@msgru.com>
2022-01-14 14:17:02 +0300

    Create ImageAdapter

---
+++ Adapter/ImageAdapter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Msgframework\Lib\Image\Adapter;
+
+use Msgframework\Lib\File\File;
+
+class ImageAdapter extends File
+{
+    const IMAGE_SITE_AUTO = 0;
+    const IMAGE_SITE_WIDTH = 1;
+    const IMAGE_SITE_HEIGHT = 2;
+}
